d437b6bef1f34bf59574bc43ab4c2a45195af43e
JAOUAD OUXHAIB s306971 <s306971@studenti.polito.it>
2024-10-12 11:22:58 +0100

    now cloudy worky

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import stylesheet from "./globals.css";
+import "./globals.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: stylesheet,
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
 
